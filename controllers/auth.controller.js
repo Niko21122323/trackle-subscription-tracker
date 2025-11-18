70dc2388a9ef94376ac0639a737fb5e1a4945acf
Nikola Stojanovski <nikola211223232529321@gmail.com>
2025-11-19 00:08:12 +0100

    working on the reminder workflow and email sender

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -87,4 +87,14 @@ export const signIn = async (req, res, next) => {
   }
 };
 
-export const signOut = async (req, res, next) => {};
+export const signOut = async (req, res, next) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "User signed out successfully",
+      data: null,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
